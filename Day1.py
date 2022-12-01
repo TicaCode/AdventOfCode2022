88ad38cf2bb4b2b9089d7e521d0d24a6e4c5d060
TicaCode <tica.schoenmakers@gmail.com>
2022-12-01 18:18:37 +0100

    Day1 - Cleaner code

--- Day1.py
+++ Day1.py
@@ -5,18 +5,13 @@ FileReader = ReadFile('InputFiles/InputDay1.txt', 'blank_lines', True)
 input_lines = FileReader.get_input()
 
 totals = [sum(x) for x in input_lines]
-print(f'Answer part 1: {max(totals)}')
 
 
 def find_sum_max_n(totals: List, n: int) -> int:
     total_list = totals.copy()
-    sum_max = 0
-    for i in range(n):
-        max_number = max(total_list)
-        max_index = total_list.index(max_number)
-        total_list = total_list[0:max_index] + total_list[max_index + 1:]
-        sum_max += max_number
-    return sum_max
+    total_list.sort(reverse=True)
+    return sum(total_list[0:n])
 
 
+print(f'Answer part 1: {find_sum_max_n(totals, 1)}')
 print(f'Answer part 2: {find_sum_max_n(totals, 3)}')
